5f27cc246d2d2eae10269f66fce7221919cc258d
Hauke Wesselmann <hauke@h-dawg.de>
2017-09-18 19:37:00 +0200

    add some notes for future development

--- app/src/main/kotlin/de/hdawg/tennis/tracker/model/Outcome.kt
+++ app/src/main/kotlin/de/hdawg/tennis/tracker/model/Outcome.kt
@@ -1,5 +1,6 @@
 package de.hdawg.tennis.tracker.model
 
 enum class Outcome {
+    // TODO split into service and in-game outcomes, add missing, remove nonesens :-)
     ACE, SECOND, DOUBLEFAULT, INPLAY, FORCEDERROR, UNFORCEDERROR, WINNER, BACKHAND, FOREHAND, NET
 }
